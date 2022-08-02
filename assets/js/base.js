6678e01efd3d96346a4a407df5f63cacbf5cea0b
lstarwin <dztree@163.com>
2022-08-02 15:21:06 +0800

    complete index

--- assets/js/base.js
+++ assets/js/base.js
@@ -3,4 +3,20 @@ var basePath = 'http://www.liulongbin.top:3007';
 //每次ajax请求前都要调用一次该函数,option就是请求的参数对象
 $.ajaxPrefilter(function(option) {
     option.url = basePath + option.url;
+    //为有权限接口[/my/]统一设置 Authorization
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //未登陆并授权用户返回处理
+    option.complete = function(res) {
+        //登陆不成功
+        if (res.responseJSON.status === 1) {
+            //强制删除localStorage
+            localStorage.removeItem('token');
+            //返回登陆页面
+            location.href = '/login.htm';
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+$(function() {
+        getUserInfo();
+        $('#btnLogout').on('click', function() {
+            layer.confirm('确定要退出码?', {
+                icon: 3,
+                title: '提示'
+            }, function(index) {
+                //清空localStorage中的token
+                localStorage.removeItem('token');
+                //跳转到登陆页面
+                location.href = '/login.htm';
+                layer.close(index);
+            });
+        })
+    })
+    //获取用户信息
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        method: 'get',
+        success: function(res) {
+            if (!res.status) {
+                return renderAvadar(res.data);
+            }
+            layui.layer.msg('获取用户信息失败!!!')
+        }
+    })
+}
+
+function renderAvadar(user) {
+    var name = user.nickname || user.username;
+    $('.welcome').html('您好,&nbsp;' + name);
+    if (user.user_pic) {
+        $('.avatar').hide();
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+    } else {
+        $('.avatar').show().html(name[0].toUpperCase());
+        $('.layui-nav-img').hide();
+    }
+}

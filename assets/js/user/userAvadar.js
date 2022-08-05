6d0473cd6ff07219ce092722a2c42d93a891ec55
lstarwin <dztree@163.com>
2022-08-05 11:52:18 +0800

    complete user model

--- assets/js/user/userAvadar.js
+++ assets/js/user/userAvadar.js
@@ -12,14 +12,39 @@ $(function() {
         // 1.3 创建裁剪区域
     $image.cropper(options);
     /***/
-    // $('input[type=file]').hide();
+    $('input[type=file]').hide();
     $('#btnUpload').on('click', function() {
-        $('input[type=file]').click();
-    })
+            $('input[type=file]').click();
+        })
+        //选择上传图片并更新图片框中图片
     $('input[type=file]').on('change', function(e) {
-        var fileList = e.target.files;
-        if (fileList.length < 1) {
-            return layer.msg('未选任何文件!!!');
-        }
+            var fileList = e.target.files;
+            if (fileList.length < 1) {
+                return layer.msg('未选任何文件!!!');
+            }
+            var fileUrl = URL.createObjectURL(fileList[0]);
+            $image.cropper('destroy').attr('src', fileUrl).cropper(options);
+        })
+        //上传图片
+    $('#btnUploadOk').on('click', function() {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布        width: 100,        height: 100      })
+                width: 100,
+                height: 100
+            }).toDataURL('image/png');
+        $.ajax({
+            method: 'post',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function(res) {
+                if (res.status) {
+                    return layer.msg(res.message);
+                }
+                window.parent.getUserInfo();
+                layer.msg(res.message);
+            }
+        })
     })
 })
